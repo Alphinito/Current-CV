d574b0779dbb1f63aa41d053ed66f2b679ebcbad
Angel Herrera <kingdocer@outlook.com>
2024-09-22 21:54:19 -0500

    Blank experience card
    Sección de experiencia laboral integrando acordeón [sin contenido]

--- src/components/experienceCard/ExperienceCard.jsx
+++ src/components/experienceCard/ExperienceCard.jsx
@@ -1,22 +1,50 @@
 import './ExperienceCard.css'
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
 
 const ExperienceCard = () => {
     //-------------------------------------------------------------------------------|>>|VARS
-    let [isFlipped, setIsFlipped] = useState(false)
+    let [isOpen, setIsOpen] = useState(false)
     //-------------------------------------------------------------------------------|>>|FUNCTIONS
     const handleClick = () => {
-        setIsFlipped(!isFlipped)
+        setIsOpen(!isOpen)
     }
+    useEffect(() => {
+        const arrowElement = document.querySelector('#arrow-id');
+        const titleCont = document.querySelector('#title-cont-id');
+        if (isOpen) {
+            arrowElement.classList.add('open');
+            arrowElement.classList.remove('closed');
+            titleCont.classList.add('open')
+            titleCont.classList.remove('closed')
+        } else {
+            arrowElement.classList.add('closed');
+            arrowElement.classList.remove('open');
+            titleCont.classList.add('closed')
+            titleCont.classList.remove('open')
+        }
+    }, [isOpen]);
     //-------------------------------------------------------------------------------|>>|DRAw
     return(
-        <article class="exp-card">
-            <h2 class="title">Expreriencia</h2>
-            <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="23.828" height="23.66"><path d="m11.817 18.021-6.161-6.083 6.179-6.262 1.579 1.579 2.828-2.828L11.817 0 0 11.976 11.835 23.66l4.405-4.405-2.828-2.828-1.595 1.594z"/><path d="M21.811 9.841h-.099l-2.884-2.884v2.884h-7.017v4h7.017v3.116l5-5-2.017-2.018v-.098z"/></svg>
-            <div className="body">
-                
-            </div>
-        </article>
+        <details class="exp-card">
+            <summary class="" onClick={handleClick}>
+                <div class="title-cont closed" id="title-cont-id">
+                    <h2 class="title">Experience</h2>
+                    <svg class="arrow closed" id="arrow-id" xmlns="http://www.w3.org/2000/svg" width="23.828" height="23.66"><path d="m11.817 18.021-6.161-6.083 6.179-6.262 1.579 1.579 2.828-2.828L11.817 0 0 11.976 11.835 23.66l4.405-4.405-2.828-2.828-1.595 1.594z"/><path d="M21.811 9.841h-.099l-2.884-2.884v2.884h-7.017v4h7.017v3.116l5-5-2.017-2.018v-.098z"/></svg>
+                </div>
+            </summary>
+            <ul class="body-exp-card">
+                <li>
+                    <div class="company">
+                        <h3 class="company-name">CGA</h3>
+                    </div>
+                </li>
+                <li>
+                    <div class="company">
+                        <h3 class="company-name">MELI</h3>
+                    </div>
+                </li>
+            </ul>
+        </details>
     )
 }
 
