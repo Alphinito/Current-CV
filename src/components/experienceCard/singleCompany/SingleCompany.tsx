563d9661dc7229e0987d2f6c36093361c167df0c
Flowtrex Dev <99404040+Alphinito@users.noreply.github.com>
2026-08-17 01:02:22 -0500

    Add experience details

--- src/components/experienceCard/singleCompany/SingleCompany.tsx
+++ src/components/experienceCard/singleCompany/SingleCompany.tsx
@@ -1,6 +1,15 @@
 import './SingleCompany.css'
 
-const SingleCompany = ({companyName, description, img, dateInit, dateFinal}) => {
+interface SingleCompanyProps {
+    companyName: string;
+    description: string;
+    skillsAndTools?: Array<string>;
+    img: string;
+    dateInit: string;
+    dateFinal: string;
+}
+
+const SingleCompany = ({companyName, description, skillsAndTools, img, dateInit, dateFinal}: SingleCompanyProps) => {
     //-------------------------------------------------------------------------------|>>|VARS
     
     //-------------------------------------------------------------------------------|>>|FUNCTIONS
@@ -11,11 +20,16 @@ const SingleCompany = ({companyName, description, img, dateInit, dateFinal}) => 
             <div class="company">
                 <div className="companyHeader">
                     <h3 class="company-name">{companyName}</h3>
-                    <time class="company-time" datetime={Date.parse(dateInit)}>{`${dateInit} - ${dateFinal}`}</time>
+                    <time class="company-time" datetime={String(Date.parse(dateInit))}>{`${dateInit} - ${dateFinal}`}</time>
                 </div>
                 <img class="company-logo" src={img} alt="company logo" />
                 <div className="companyDescription">
                     {description}
+                </div>
+                <div className="companySkillsAndToolsContainer">
+                    {skillsAndTools?.map(skillOrTool => (
+                        <span className="company-skill-or-tool">{skillOrTool}</span>
+                    ))}
                 </div>
             </div>
         </li>
