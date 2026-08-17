264232b2eee258367196e4a0c6fe28efa44a69a5
Flowtrex Dev <99404040+Alphinito@users.noreply.github.com>
2026-08-17 03:29:20 -0500

    base location updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alphinito.github.io',
+  base: isProduction ? '/Current-CV' : undefined,
   integrations: [preact()]
 });
